d272d0e92c622f250d69d8e50c203ef438d22798
Julian Blaskiewicz <julian.blaskiewicz@outlook.com>
2021-06-21 21:11:17 +0100

    initial

---
+++ src/main/java/com/jblaski/unihelpermodel/UniHelperModelApplication.java
@@ -0,0 +1,13 @@
+package com.jblaski.unihelpermodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniHelperModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniHelperModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jblaski/unihelpermodel/UniHelperModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jblaski.unihelpermodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniHelperModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
